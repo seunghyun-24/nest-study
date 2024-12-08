a83561651cfe4b42c9da1c1821bee737415c0967
seunghyun-24 <hyunlight@korea.ac.kr>
2024-12-09 08:22:04 +0900

    add delete func to auth

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,13 @@
-import { Controller, Delete, HttpCode, Param } from '@nestjs/common';
+import { Controller, Delete, HttpCode, Param, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
-import { ApiNoContentResponse, ApiOperation } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiNoContentResponse,
+  ApiOperation,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
+import { CurrentUser } from '../auth/decorator/user.decorator';
+import { UserBaseInfo } from 'src/auth/type/user-base-info.type';
 
 @Controller('users')
 export class UserController {
@@ -8,9 +15,14 @@ export class UserController {
 
   @Delete(':userId')
   @HttpCode(204)
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: '유저 탈퇴' })
   @ApiNoContentResponse()
-  async deleteUser(@Param('userId') userId: number): Promise<void> {
-    return this.userService.deleteUser(userId);
+  async deleteUser(
+    @Param('userId') userId: number,
+    @CurrentUser() user: UserBaseInfo,
+  ): Promise<void> {
+    return this.userService.deleteUser(userId, user);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,15 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
+import { UserBaseInfo } from '../auth/type/user-base-info.type';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async deleteUser(userId: number): Promise<void> {
-    const user = await this.userRepository.getUserById(userId);
-
-    if (!user) {
-      throw new NotFoundException('User not found');
+  async deleteUser(userId: number, user: UserBaseInfo): Promise<void> {
+    if (userId !== user.id) {
+      throw new BadRequestException('타인의 계정은 삭제할 수 없습니다.');
     }
-
     return this.userRepository.deleteUser(userId);
   }
 }
